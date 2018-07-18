6c6c8bae6dc6ab27f877ad040f2cdc9ef5de11dc
NitinBuzz <reachnitinvenkat@gmail.com>
2018-07-18 22:40:12 +0530

    Flexi init

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+export default class Flexi extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term1: '',
+      id1: '',
+      title1: '',
+      term2: '',
+      id2: '',
+      title2: ''
+    };
+  }
+
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <form>
+          {this.props.config.items.map((key, value) => {
+            return (
+              <div>
+                <label>{key.label} : </label>
+                {key.type == 'DropDown' ? (
+                  <select>
+                    {key.values.map(v => (
+                      <option key={v} value={v}>
+                        {' '}
+                        {v}
+                      </option>
+                    ))}
+                  </select>
+                ) : (
+                  <input type={key.type} />
+                )}
+                <p />
+              </div>
+            );
+          })}
+        </form>
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+import Flexi from './components/app';
 import reducers from './reducers';
+
+const flexiConfig = {
+  items: [
+    {
+      name: 'personname',
+      label: "Person's Name",
+      type: 'TextField'
+    },
+    {
+      name: 'states',
+      label: "Person's state",
+      type: 'DropDown',
+      values: ['Maharashtra', 'Kerala', 'Tamil Nadu']
+    }
+  ]
+};
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Flexi config={flexiConfig} />
+  </Provider>,
+  document.querySelector('.container')
+);
